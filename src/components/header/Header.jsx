d2acb58dd0e62e9e4874802de3359c4a775acba1
OMID GHARAVI <omidbros3@gmail.com>
2024-07-31 01:27:01 +0330

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false)
+
     return (
-        <div>header</div>
+        <div className='w-full h-[70px] bg-[#191919] flex items-center justify-between px-3'>
+            <div className='w-14 h-14 rounded-full bg-zinc-600'>
+                {/* <Image src={''} alt='profile' /> */}
+            </div>
+            <div
+                className='relative cursor-pointer w-28 h-12 rounded-3xl bg-zinc-600 flex justify-between items-center'
+                onClick={() => setToggle(!toggle)}
+            >
+                <div
+                    className={`z-20 absolute w-10 h-10 rounded-full bg-white transition-all duration-300 ease-in-out 
+                        ${toggle ? 'left-[68px]' : 'left-1'}`}
+                ></div>
+                <p className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-100' : 'opacity-0'}`}>en</p>
+                <p className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-0' : 'opacity-100'}`}>fa</p>
+            </div>
+        </div>
     )
 }
 
