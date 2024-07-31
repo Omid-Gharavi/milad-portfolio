ebc59471e417937c99447fb43c4d6aa5b6e3ae59
OMID GHARAVI <omidbros3@gmail.com>
2024-07-31 12:01:26 +0330

    Hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1f1f1f]">
       <Header />
+      <Hero />
     </main>
   );
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className='fixed top-40 inset-x-0 flex flex-col items-center'>
+            <p className='text-7xl font-bold text-white'>Milad Gharavi</p>
+            <p className='mt-8 text-zinc-200 text-lg'>Architecture</p>
+        </div>
     )
 }
 
