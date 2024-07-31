34f6ebdf3902f94c351d185e20d01bf038e62da3
OMID GHARAVI <omidbros3@gmail.com>
2024-07-31 12:03:18 +0330

    add: sample component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import Sample from "@/components/samples/Sample";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1f1f1f]">
       <Header />
       <Hero />
+      <Sample />
     </main>
   );
 }
