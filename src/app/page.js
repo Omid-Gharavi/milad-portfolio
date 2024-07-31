58a8431cfc3b47a6d619b4c3cd181e46d6275e57
OMID GHARAVI <omidbros3@gmail.com>
2024-07-31 11:49:05 +0330

    add: hero component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-[#1f1f1f]">
       <Header />
     </main>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const [toggle, setToggle] = useState(false)
 
     return (
-        <div className='w-full h-[70px] bg-[#191919] flex items-center justify-between px-3'>
+        <div className='w-full h-[70px] bg-[#191919] flex items-center justify-between px-3 sticky top-0'>
             <div className='w-14 h-14 rounded-full bg-zinc-600'>
                 {/* <Image src={''} alt='profile' /> */}
             </div>
@@ -18,8 +18,8 @@ const Header = () => {
                     className={`z-20 absolute w-10 h-10 rounded-full bg-white transition-all duration-300 ease-in-out 
                         ${toggle ? 'left-[68px]' : 'left-1'}`}
                 ></div>
-                <p className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-100' : 'opacity-0'}`}>en</p>
-                <p className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-0' : 'opacity-100'}`}>fa</p>
+                <p className={`text-white absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-100' : 'opacity-0'}`}>en</p>
+                <p className={`text-white absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 ease-in-out ${toggle ? 'opacity-0' : 'opacity-100'}`}>fa</p>
             </div>
         </div>
     )
